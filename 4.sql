2544f4abec7446d59dd7527da323dfd7d6b1beab
Anna <70943933+Ann01Zadoiko@users.noreply.github.com>
2022-08-31 09:45:41 +0300

    Delete 4.sql

--- 4.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE public.projects
-ADD cost int;
-
-update public.projects set cost = 40000 where id_project = 31;
-update public.projects set cost = 40000 where id_project = 32;
-update public.projects set cost= 40000 where id_project = 33;
-update public.projects set cost = 40000 where id_project = 34;
